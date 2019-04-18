9cc4d152b1a59b87b953a6cb13a4b1967e650437
Mauro Moura <devmauromoura@gmail.com>
2019-04-18 16:24:15 -0300

    Fix Alert Login 2

--- application/app/Http/Controllers/UserController.php
+++ application/app/Http/Controllers/UserController.php
@@ -50,7 +50,7 @@ class UserController extends Controller
             }
         }
         else{
-            Redirect('index')->with('alert', 'Usuário/Senha incorreto!');
+            return redirect('index')->with('alert', 'Usuário/Senha incorreto!');
         }
     }
 
