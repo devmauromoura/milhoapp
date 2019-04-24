c71e0e872a05f451d6e7c389724d4a6c14a81091
Mauro Moura <devmauromoura@gmail.com>
2019-04-24 02:25:47 -0300

    Created Migration Voto Fasipe and created routes/functions

--- application/app/Http/Controllers/gestaoFasipe.php
+++ application/app/Http/Controllers/gestaoFasipe.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-Use App\Barraca;
+use App\Barraca;
+use App\gestaoVotosFasipe;
 
 class gestaoFasipe extends Controller
 {
     public function index(){
         return Barraca::all();
+
+        //Deverá ser listado a Barraca e seus pontos individuas, assim como um totalizador.
     }
 
     public function barraca($id){
@@ -19,11 +22,13 @@ class gestaoFasipe extends Controller
 
     public function enviarNotas(Request $request){
         $dados = $request->all();
-        $votoFasipe = new Barraca;
+        $votoFasipe = new gestaoVotosFasipe;
         $votoFasipe->vendaingresso = $dados['vendaingresso'];
         $votoFasipe->dadosaplicativo = $dados['dadosaplicativo'];
         $votoFasipe->workshop = $dados['workshop'];
         $votoFasipe->reuniaovigilancia = $dados['reuniaovigilancia'];
+        $votoFasipe->idbarraca = $dados['idbarraca'];
+        $votoFasipe->idusuario = Auth::user(id);
         $votoFasipe->save();
         return redirect()->back()->with('Alert','Voto(s) inserido(s) com sucesso!');
     }
---
+++ application/app/gestaoVotosFasipe.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class gestaFasipe extends Model
+{
+    protected $table = "gestaofasipe";
+}
