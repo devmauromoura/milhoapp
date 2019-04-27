5f37f9b8c2f89c2e76e6335938212598cfc6fa20
Mauro Moura <devmauromoura@gmail.com>
2019-04-27 01:41:12 -0300

    Fix migration cursos - add all

--- application/database/migrations/2019_02_21_041730_create_curso_table.php
+++ application/database/migrations/2019_02_21_041730_create_curso_table.php
@@ -19,7 +19,25 @@ class CreateCursoTable extends Migration
             $table->timestamps();
         });
 
-        DB::table('curso')->insert(['nome' => 'Default']);
+        DB::table('curso')->insert(['nome' => '']);
+        DB::table('curso')->insert(['nome' => 'Administração']);
+        DB::table('curso')->insert(['nome' => 'Análise e Desenvolvimento de Sistemas']);
+        DB::table('curso')->insert(['nome' => 'Arquitetura e Urbanismo']);
+        DB::table('curso')->insert(['nome' => 'Biomedicina']);
+        DB::table('curso')->insert(['nome' => 'Ciências Contábeis']);
+        DB::table('curso')->insert(['nome' => 'Direito']);
+        DB::table('curso')->insert(['nome' => 'Educação Física']);
+        DB::table('curso')->insert(['nome' => 'Enfermagem']);
+        DB::table('curso')->insert(['nome' => 'Engenharia Civil']);
+        DB::table('curso')->insert(['nome' => 'Engenharia de Produção']);
+        DB::table('curso')->insert(['nome' => 'Estética e Cosmética']);
+        DB::table('curso')->insert(['nome' => 'Farmácia']);
+        DB::table('curso')->insert(['nome' => 'Fisioterapia']);
+        DB::table('curso')->insert(['nome' => 'Gastronomia']);
+        DB::table('curso')->insert(['nome' => 'Jornalismo']);
+        DB::table('curso')->insert(['nome' => 'Nutrição']);
+        DB::table('curso')->insert(['nome' => 'Odontologia']);
+        DB::table('curso')->insert(['nome' => 'Psicologia']);
     }
 
     /**
