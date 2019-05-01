d402fd9e7ef6cb6890b6c60e412588d326150771
Mauro Moura <devmauromoura@gmail.com>
2019-05-01 11:28:12 -0300

    Fix HASH Login

--- application/app/Http/Controllers/Auth/LoginController.php
+++ application/app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
